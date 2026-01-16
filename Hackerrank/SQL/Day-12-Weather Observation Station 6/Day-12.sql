4ad3c5648dd075bcdc4d8f7b464f549b1c078fd8
C M Aswin <166792970+cmaswin@users.noreply.github.com>
2026-01-16 15:55:01 +0530

    Add files via upload

---
+++ Hackerrank/SQL/Day-12-Weather Observation Station 6/Day-12.sql
@@ -0,0 +1,150 @@
+-- Query the list of CITY names starting with vowels 
+-- (i.e., a, e, i, o, or u) from STATION. 
+-- Your result cannot contain duplicates.
+-- Input Format
+-- The STATION table is described as follows:
+
+-- STATION
+-- -----------------------------
+-- ID       NUMBER
+-- CITY     VARCHAR2(21)
+-- STATE    VARCHAR2(2)
+-- LAT_N    NUMBER
+-- LONG_W   NUMBER
+
+-- Expected OUTPUT
+-- Arlington
+-- Albany
+-- Upperco
+-- Aguanga
+-- Odin
+-- East China
+-- Algonac
+-- Onaway
+-- Irvington
+-- Arrowsmith
+-- Udall
+-- Oakfield
+-- Elkton
+-- East Irvine
+-- Amo
+-- Alanson
+-- Eleele
+-- Auburn
+-- Oconee
+-- Amazonia
+-- Aliso Viejo
+-- Andersonville
+-- Eros
+-- Arkadelphia
+-- Eriline
+-- Edgewater
+-- East Haddam
+-- Eastlake
+-- Addison
+-- Everton
+-- Eustis
+-- Arispe
+-- Union Star
+-- Ottertail
+-- Ermine
+-- Albion
+-- Athens
+-- Eufaula
+-- Osage City
+-- Andover
+-- Osborne
+-- Elm Grove
+-- Atlantic Mine
+-- Oshtemo
+-- Archie
+-- Olmitz
+-- Allerton
+-- Equality
+-- Alpine
+-- Ojai
+-- Orange Park
+-- Urbana
+-- Ukiah
+-- Alba
+-- Esmond
+-- Eureka Springs
+-- Eskridge
+-- Ozona
+-- Orange City
+-- Effingham
+-- Alton
+-- Agency
+-- Anthony
+-- Emmett
+-- Acme
+
+select distinct CITY from STATION where 
+lower(substr(CITY,1,1)) in ('a','e','i','o','u');
+
+-- Your Output (stdout)
+-- Arlington
+-- Albany
+-- Upperco
+-- Aguanga
+-- Odin
+-- East China
+-- Algonac
+-- Onaway
+-- Irvington
+-- Arrowsmith
+-- Udall
+-- Oakfield
+-- Elkton
+-- East Irvine
+-- Amo
+-- Alanson
+-- Eleele
+-- Auburn
+-- Oconee
+-- Amazonia
+-- Aliso Viejo
+-- Andersonville
+-- Eros
+-- Arkadelphia
+-- Eriline
+-- Edgewater
+-- East Haddam
+-- Eastlake
+-- Addison
+-- Everton
+-- Eustis
+-- Arispe
+-- Union Star
+-- Ottertail
+-- Ermine
+-- Albion
+-- Athens
+-- Eufaula
+-- Osage City
+-- Andover
+-- Osborne
+-- Elm Grove
+-- Atlantic Mine
+-- Oshtemo
+-- Archie
+-- Olmitz
+-- Allerton
+-- Equality
+-- Alpine
+-- Ojai
+-- Orange Park
+-- Urbana
+-- Ukiah
+-- Alba
+-- Esmond
+-- Eureka Springs
+-- Eskridge
+-- Ozona
+-- Orange City
+-- Effingham
+-- Alton
+-- Agency
+-- Anthony
+-- Emmett
+-- Acme
