82f2a008cd96023c500e657822b4896f16e3115f
C M Aswin <166792970+cmaswin@users.noreply.github.com>
2026-01-22 22:16:56 +0530

    Add files via upload

---
+++ Day-21-Employee Names/Day-21.sql
@@ -0,0 +1,157 @@
+-- Write a query that prints a list of employee names 
+-- (i.e.: the name attribute) from the Employee table in 
+-- alphabetical order.
+
+-- Input Format
+
+-- The Employee table containing employee data for a company 
+-- is described as follows:
+
+-- EMPLOYEE
+-- ------------------------------------------------
+-- Field          | Type
+-- ------------------------------------------------
+-- EMPLOYEE_ID    | INTEGER
+-- NAME           | STRING
+-- MONTHS         | INTEGER
+-- SALARY         | INTEGER
+-- ------------------------------------------------
+
+-- where employee_id is an employee's ID number, name is their name, 
+-- months is the total number of months they've been working for the 
+-- company, and salary is their monthly salary.
+
+-- Sample Input
+
+-- EMPLOYEE
+-- ------------------------------------------------------------
+-- EMPLOYEE_ID | NAME     | MONTHS | SALARY
+-- ------------------------------------------------------------
+-- 12228       | Rose     | 15     | 1968
+-- 33645       | Angela   | 1      | 3443
+-- 45692       | Frank    | 17     | 1608
+-- 56118       | Patrick  | 7      | 1345
+-- 59725       | Lisa     | 11     | 2330
+-- 74197       | Kimberly | 16     | 4372
+-- 78454       | Bonnie   | 8      | 1771
+-- 83565       | Michael  | 6      | 2017
+-- 98607       | Todd     | 5      | 3396
+-- 99989       | Joe      | 9      | 3573
+-- ------------------------------------------------------------
+
+-- Sample Output
+
+-- Angela
+-- Bonnie
+-- Frank
+-- Joe
+-- Kimberly
+-- Lisa
+-- Michael
+-- Patrick
+-- Rose
+-- Todd
+
+select name from Employee order by name asc;
+
+-- Your Output (stdout)
+-- Alan 
+-- Amy 
+-- Andrew 
+-- Andrew 
+-- Angela 
+-- Ann 
+-- Anna 
+-- Anthony 
+-- Antonio 
+-- Benjamin 
+-- Bonnie 
+-- Brandon 
+-- Brandon 
+-- Brian 
+-- Carol 
+-- Charles 
+-- Christina 
+-- Christina 
+-- Christine 
+-- Christine 
+-- David 
+-- David 
+-- David 
+-- Debra 
+-- Diana 
+-- Diana 
+-- Donna 
+-- Dorothy 
+-- Dorothy 
+-- Dorothy 
+-- Earl 
+-- Elizabeth 
+-- Emily 
+-- Evelyn 
+-- Evelyn 
+-- Frank 
+-- Gerald 
+-- Gloria 
+-- Gregory 
+-- Harry 
+-- Jacqueline 
+-- James 
+-- James 
+-- Jean 
+-- Jennifer 
+-- Jesse 
+-- Jesse 
+-- Joe 
+-- Joe 
+-- John 
+-- John 
+-- Jonathan 
+-- Joseph 
+-- Joyce 
+-- Joyce 
+-- Julia 
+-- Keith 
+-- Kelly 
+-- Kevin 
+-- Kimberly 
+-- Kimberly 
+-- Lawrence 
+-- Lillian 
+-- Linda 
+-- Lisa 
+-- Lori 
+-- Louise 
+-- Maria 
+-- Marilyn 
+-- Marilyn 
+-- Marilyn 
+-- Martha 
+-- Martin 
+-- Matthew 
+-- Melissa 
+-- Michael 
+-- Michelle 
+-- Nancy 
+-- Norma 
+-- Pamela 
+-- Patricia 
+-- Patrick 
+-- Paul 
+-- Paul 
+-- Paula 
+-- Paula 
+-- Phillip 
+-- Ralph 
+-- Robert 
+-- Robin 
+-- Robin 
+-- Rose 
+-- Roy 
+-- Stephanie 
+-- Tammy 
+-- Timothy 
+-- Todd 
+-- Victor 
+-- Walter 
+-- Willie 
